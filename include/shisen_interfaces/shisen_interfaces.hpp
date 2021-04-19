b34f81ef50e338b3949d04ab978254086c01d86d
Nathan24zz <nathanterbaik@gmail.com>
2021-04-19 23:20:11 +0700

    Add Master Header

---
+++ include/shisen_interfaces/shisen_interfaces.hpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Ichiro ITS
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef SHISEN_INTERFACES__SHISEN_INTERFACES_HPP_
+#define SHISEN_INTERFACES__SHISEN_INTERFACES_HPP_
+
+#include <shisen_interfaces/msg/compressed_image.hpp>
+#include <shisen_interfaces/msg/property.hpp>
+#include <shisen_interfaces/msg/property_event.hpp>
+#include <shisen_interfaces/msg/raw_image.hpp>
+#include <shisen_interfaces/srv/get_properties.hpp>
+#include <shisen_interfaces/srv/set_properties.hpp>
+
+#endif  // SHISEN_INTERFACES__SHISEN_INTERFACES_HPP_
